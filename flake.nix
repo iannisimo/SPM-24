0d8469884506e7321b7d4371af3327bcbbfd7703
Simone <iannisimo@gmail.com>
2024-06-13 15:49:05 +0200

    move to gcc 13

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A template for Nix based C++ project setup.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
 
     utils.url = "github:numtide/flake-utils";
     utils.inputs.nixpkgs.follows = "nixpkgs";
