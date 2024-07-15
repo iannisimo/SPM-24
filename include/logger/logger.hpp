c8c69403be3bfe49955de20010ee09a748d14aad
Simone <iannisimo@gmail.com>
2024-07-15 11:51:33 +0200

    logger helper for mpi

--- include/logger/logger.hpp
+++ include/logger/logger.hpp
@@ -2,6 +2,7 @@
 #define _LOGGER
 
 #include <iostream>
+#include <string>
 
 int LOGGER_QUIET = 0;
 
@@ -11,31 +12,37 @@ bool debug = true;
 bool debug = false;
 #endif
 
+std::string prefix = "";
+
+void setPrefix(std::string pfx) {
+  prefix = pfx;
+}
+
 
 void LOG_E(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 2) return;
   if (tag[0] != 0) {
-    std::cerr << tag << ": " << msg << std::endl;
+    std::cerr << prefix << tag << ": " << msg << std::endl;
   } else {
-    std::cerr << msg << std::endl;
+    std::cerr << prefix << msg << std::endl;
   }
 }
 
 void LOG_W(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 1) return;
   if (tag[0] != 0) {
-    std::cout << tag << ": " << msg << std::endl;
+    std::cout << prefix << tag << ": " << msg << std::endl;
   } else {
-    std::cout << msg << std::endl;
+    std::cout << prefix << msg << std::endl;
   }
 }
 
 void LOG_I(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 0) return;
   if (tag[0] != 0) {
-    std::cout << tag << ": " << msg << std::endl;
+    std::cout << prefix << tag << ": " << msg << std::endl;
   } else {
-    std::cout << msg << std::endl;
+    std::cout << prefix << msg << std::endl;
   }
 }
 
@@ -43,18 +50,18 @@ void LOG_D(const std::string tag, const std::string msg) {
   if (!debug) return;
   if (LOGGER_QUIET > 3) return;
   if (tag[0] != 0) {
-    std::cout << tag << ": " << msg << std::endl;
+    std::cout << prefix << tag << ": " << msg << std::endl;
   } else {
-    std::cout << msg << std::endl;
+    std::cout << prefix << msg << std::endl;
   }
 }
 
 void LOG_T(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 4) return;
   if (tag[0] != 0) {
-    std::cout << tag << ": " << msg << std::endl;
+    std::cout << prefix << tag << ": " << msg << std::endl;
   } else {
-    std::cout << msg << std::endl;
+    std::cout << prefix << msg << std::endl;
   }
 }
 
