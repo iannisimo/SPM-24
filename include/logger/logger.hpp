dc6385f323f1537a14fb2ca96dc369ce2a2e70ec
Simone <iannisimo@gmail.com>
2024-07-18 10:47:25 +0200

    Revert "logger helper for mpi"
    This reverts commit c8c69403be3bfe49955de20010ee09a748d14aad.

--- include/logger/logger.hpp
+++ include/logger/logger.hpp
@@ -2,7 +2,6 @@
 #define _LOGGER
 
 #include <iostream>
-#include <string>
 
 int LOGGER_QUIET = 0;
 
@@ -12,37 +11,31 @@ bool debug = true;
 bool debug = false;
 #endif
 
-std::string prefix = "";
-
-void setPrefix(std::string pfx) {
-  prefix = pfx;
-}
-
 
 void LOG_E(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 2) return;
   if (tag[0] != 0) {
-    std::cerr << prefix << tag << ": " << msg << std::endl;
+    std::cerr << tag << ": " << msg << std::endl;
   } else {
-    std::cerr << prefix << msg << std::endl;
+    std::cerr << msg << std::endl;
   }
 }
 
 void LOG_W(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 1) return;
   if (tag[0] != 0) {
-    std::cout << prefix << tag << ": " << msg << std::endl;
+    std::cout << tag << ": " << msg << std::endl;
   } else {
-    std::cout << prefix << msg << std::endl;
+    std::cout << msg << std::endl;
   }
 }
 
 void LOG_I(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 0) return;
   if (tag[0] != 0) {
-    std::cout << prefix << tag << ": " << msg << std::endl;
+    std::cout << tag << ": " << msg << std::endl;
   } else {
-    std::cout << prefix << msg << std::endl;
+    std::cout << msg << std::endl;
   }
 }
 
@@ -50,18 +43,18 @@ void LOG_D(const std::string tag, const std::string msg) {
   if (!debug) return;
   if (LOGGER_QUIET > 3) return;
   if (tag[0] != 0) {
-    std::cout << prefix << tag << ": " << msg << std::endl;
+    std::cout << tag << ": " << msg << std::endl;
   } else {
-    std::cout << prefix << msg << std::endl;
+    std::cout << msg << std::endl;
   }
 }
 
 void LOG_T(const std::string tag, const std::string msg) {
   if (LOGGER_QUIET > 4) return;
   if (tag[0] != 0) {
-    std::cout << prefix << tag << ": " << msg << std::endl;
+    std::cout << tag << ": " << msg << std::endl;
   } else {
-    std::cout << prefix << msg << std::endl;
+    std::cout << msg << std::endl;
   }
 }
 
